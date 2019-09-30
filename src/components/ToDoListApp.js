b075c3867a2bbe4bff42e781ab57c04d1e71d0b0
Malga90 <savemeebarry@gmail.com>
2019-09-30 23:27:05 +0200

    bootsrap installed

--- src/components/ToDoListApp.js
+++ src/components/ToDoListApp.js
@@ -4,7 +4,9 @@ export default class ToDoListApp extends React.Component {
   render() {
     return (
       <div>
-        <h1>Let's see about that!! nananna</h1>
+        <h1>To Do List</h1>
+        <input type="text"></input>
+        <button>Add!</button>
       </div>
     );
   }
---
+++ src/redux-todo.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers } from "redux";
+import uuid from "uuid";
+
+// Add todo
+// remove todo
+// edit todo
+// done todo
+
+// Todo reducer
