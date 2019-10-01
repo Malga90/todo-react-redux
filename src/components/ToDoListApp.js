38a72f137697f172e59647285ad3f8fbd59bafab
Malga90 <savemeebarry@gmail.com>
2019-10-01 22:53:02 +0200

    added style for to do list

--- src/components/ToDoListApp.js
+++ src/components/ToDoListApp.js
@@ -1,13 +1,34 @@
 import React from "react";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
-export default class ToDoListApp extends React.Component {
+class TodoList extends React.Component {
   render() {
     return (
-      <div>
-        <h1>To Do List</h1>
-        <input type="text"></input>
-        <button>Add!</button>
-      </div>
+      <Container className="mt-4">
+        <Row className="justify-content-md-center">
+          <h1>To Do List</h1>
+        </Row>
+        <Form>
+          <InputGroup className="mb-3">
+            <FormControl
+              placeholder="What would you like to do?"
+              aria-label="To do to add"
+              aria-describedby="basic-addon2"
+            />
+            <InputGroup.Append>
+              <Button type="submit" variant="info">
+                Add
+              </Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form>
+      </Container>
     );
   }
 }
+export default TodoList;
--- src/redux-todo.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers } from "redux";
-import uuid from "uuid";
-
-// Add todo
-// remove todo
-// edit todo
-// done todo
-
-// Todo reducer
