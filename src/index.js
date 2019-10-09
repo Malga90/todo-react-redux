a0a0807f876ea2d2c5caf42febede91d34664483
Malga90 <savemeebarry@gmail.com>
2019-10-09 20:34:41 +0200

    installed react-redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import ToDoListApp from "./components/ToDoListApp";
 import * as serviceWorker from "./serviceWorker";
