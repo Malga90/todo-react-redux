2a80ce237c093e4c7fb3b45dbeeb08c44d8b9a1e
Malga90 <savemeebarry@gmail.com>
2019-10-09 19:38:12 +0200

    added buttons to the list items

--- src/components/ListElement.js
+++ src/components/ListElement.js
@@ -1,14 +1,34 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 export default class ListElement extends React.Component {
   render() {
     return (
       <div>
         <ListGroup>
-          <ListGroup.Item>Get a job</ListGroup.Item>
-          <ListGroup.Item>Make espresso martini</ListGroup.Item>
-          <ListGroup.Item>Become React Ninja</ListGroup.Item>
+          <ListGroup.Item className="d-flex justify-content-between">
+            Get a job
+            <ButtonGroup>
+              <Button variant="success">Done</Button>
+              <Button variant="danger">Remove</Button>
+            </ButtonGroup>
+          </ListGroup.Item>
+          <ListGroup.Item className="d-flex justify-content-between">
+            Make espresso martini
+            <ButtonGroup>
+              <Button variant="success">Done</Button>
+              <Button variant="danger">Remove</Button>
+            </ButtonGroup>
+          </ListGroup.Item>
+          <ListGroup.Item className="d-flex justify-content-between">
+            Become React Ninja
+            <ButtonGroup>
+              <Button variant="success">Done</Button>
+              <Button variant="danger">Remove</Button>
+            </ButtonGroup>
+          </ListGroup.Item>
         </ListGroup>
       </div>
     );
